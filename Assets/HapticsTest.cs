9b69695f5cba5d2cb5404bb51b8fb09d89520295
oitvrdev <oit-vrdev@wmich.edu>
2018-07-31 17:25:54 -0400

    Haptics Script Added to Thermometer

---
+++ Assets/HapticsTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class HapticsTest : MonoBehaviour {
+
+    public AudioClip hapticsClip;
+    OVRHapticsClip _Clip;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayHaptics()
+    {
+        _Clip = new OVRHapticsClip(hapticsClip);
+        OVRHaptics.Channels[1].Queue(_Clip);
+
+    }
+}
